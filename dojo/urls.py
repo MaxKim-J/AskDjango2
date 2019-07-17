eff74c545499d8808f0b4397c8c085c6e6dd5a0c
김종혁 <hwaeen@gmail.com>
2019-07-17 17:53:01 +0900

    dojo.view.hello : 한글 문자열 매칭

--- dojo/urls.py
+++ dojo/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import re_path
 
 urlpatterns = [
-re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum)
+    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    re_path(r'^hello/(?P<name>[r-힣]+)/(?P<age>\d+)/$', views.hello)
 ]
