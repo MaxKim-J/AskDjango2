00828b577dd4e6327db9dc3886fa3ce3f91496be
김종혁 <hwaeen@gmail.com>
2019-07-20 00:22:53 +0900

    accounts.models.Profile 모델 생성/admin 등록

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model): #하나의 유저 하나의 프로필 이상은 못만듬
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
